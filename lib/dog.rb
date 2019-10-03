afdccc4d9b2b7244de757ad502fe60d745a6615e
Andrew Silva <andrewsilva9@gmail.com>
2019-10-03 00:46:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,13 @@ class Dog
     self.new(hash)
   end
 
-  def self.find_by_id
-    
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
