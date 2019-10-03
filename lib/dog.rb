9cd29f6903e446bfcbf37e8cd8aa8b0d2c317a5a
Andrew Silva <andrewsilva9@gmail.com>
2019-10-03 00:50:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,5 +67,10 @@ class Dog
     SQL
 
     dog = DB[:conn].execute(sql, name, breed).first
+
+    if dog
+      new_dog self.new_from_db(dog)
+    else
+      new_dog = self.create(:name => name, :breed => breed)
   end
 end
