5ff4e049039d0f1685ca57e59fe53fdd3595ac5f
Andrew Silva <andrewsilva9@gmail.com>
2019-10-03 00:34:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,8 +11,8 @@ class Dog
 
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY
-      name TEXT
+      id INTEGER PRIMARY KEY,
+      name TEXT,
       breed TEXT
     )
     SQL
