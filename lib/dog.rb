8ba3b4b87e2781e3a15917363c080d1024d8d47e
Andrew Silva <andrewsilva9@gmail.com>
2019-10-03 00:32:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,13 @@ class Dog
 
   attr_accessor :id, :name, :breed
 
-def initialize(attributes)
-  attributes.each {|key, value| self.send(("#{key}="), value)}
-  self.id ||= nil
-end
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
+
+  def self.create_table
+
+    sql = <<-SQL 
+  end
 end
